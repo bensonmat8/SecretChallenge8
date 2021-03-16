02c1e7802b196caf0701d68b2eaf659643ae7311
bensonmat8 <37485710+bensonmat8@users.noreply.github.com>
2021-03-16 13:52:32 -0400

    Adding challenge 2 and 3

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
+from benford import benford_test
 from flask import Flask, flash, render_template, request
+import json
+import plotly.express as px
+from plotly.utils import PlotlyJSONEncoder
 
 app = Flask(__name__)
 app.secret_key = b'This is a long test secret key'
@@ -7,9 +11,23 @@ app.secret_key = b'This is a long test secret key'
 def home():
     return render_template('home.html')
 
+def graph_plot(data_count):
+    total = sum(data_count)
+    data_count = [round(x/total*100, 1) for x in data_count]
+    x = [x for x in range(1,10)]
+    benford = [30.1, 17.6, 12.5, 9.7, 7.9, 6.7, 5.8, 5.1, 4.6]
+    fig = px.line(x=x, y=benford)
+    fig.add_bar(x=x, y=data_count, name='Observed Data')
+    fig.update_layout(xaxis_title='Digits',
+                    yaxis_title='Frequency (%)')
+    graphJSON = json.dumps(fig, cls=PlotlyJSONEncoder)
+    return graphJSON
+
+
 @app.route('/Challenge1', methods=['GET', 'POST'])
 def challenge1():
-    columns = None
+    result = None
+    plot = None
     if request.method == 'POST':
         uploaded_file = request.files['data_file']
         try:
@@ -18,10 +36,14 @@ def challenge1():
             flash('Invalid file given, please try again')
             return render_template('challenge1.html', columns=columns)
         columns = [x for x in content[0].split('\t')]
+        columns = columns + [x for x in content[0].split(',')]
         if '7_2009' not in columns:
             flash("Column '7_2009' not found in the file, please try again.")
             render_template('challenge1.html', columns=columns)
-    return render_template('challenge1.html', columns=columns)
+        result, data_count = benford_test(content)
+        plot =  graph_plot(data_count)
+    return render_template('challenge1.html', result=result
+            , plot=plot)
 
 @app.route('/Challenge2', methods=['GET', 'POST'])
 def challenge2():
--- benford.py
+++ benford.py
@@ -1,14 +1,45 @@
-def benford_test(data_set):
-    benford = [30.1, 17.6, 12.5, 9.7, 7.9, 6.7, 5.8, 5.1, 4.6]
+from math import log10
+
+def benford_test(content) -> tuple:
+    """
+    Takes the data from '7_2009' column and checks if the data
+    satisy Benford's law by doing a Chi-Squared test
+    """
+    data_set = process_data(content)
+    benford = [ 100*log10(1 + 1/n) for n in range(1, 10) ]
     total_count = 0
     data_count = [0]*9
     for i in data_set:
-        data_count[i] += 1
+        data_count[i-1] += 1
         total_count += 1
     expected_counts = [round(p * total_count / 100) for p in benford]
+    # Chi-Squared test
     chi_square_stat = 0
     for data, expected in zip(data_count,expected_counts):
         diff = data - expected
         chi_square = diff * diff
-        chi_square_stat += chi_square
-    return chi_square_stat < 15.51 #Critical value at a P-value of 0.05 is 15.51
+        chi_square_stat += chi_square / expected
+    result = chi_square_stat < 15.51 #Critical value at a P-value of 0.05 is 15.51
+    return result, data_count
+
+def process_data(content) -> list:
+    """
+    Takes the content, looks for '7_2009' column and collects the
+    first digit of the data into a list and returns the list
+    """
+    if '7_2009' in [x for x in content[0].split('\t')]:
+        sep = '\t'
+    elif '7_2009' in [x for x in content[0].split(',')]:
+        sep = ','
+    
+    for idx,i in enumerate(content[0].split(sep)):
+        if '7_2009' == i:
+            break
+    data_set = []
+    for line in content[1:]:
+        if line == '': #if the last line is empty
+            continue
+        val = line.split(sep)[idx]
+        val = val[0] # Taking the first digit of the value
+        data_set.append(int(val))
+    return data_set
